efac859daee5969eaf27f95e03ea5c79b49973a6
Hui Yu Shi <huiyu.shi@siemens.com>
2022-02-28 16:16:04 +0800

    feat(): add iteration RTC[] reviewer()
    chore(): add doxygen style comments RTC[] reviewer()

--- 1.c
+++ 1.c
@@ -1,5 +1,21 @@
+/**
+ * @file 1.c
+ *
+ * @version 1.0
+ * @date 2022-02-28
+ *
+ * Copyright (C) 1995-2022 by Beijing Siemens Cerberus Electronics Ltd.
+ */
 #include "stdio.h"
 
+/**
+ * @brief main
+ * @return int
+ */
 int main() {
-	reutrn 0;
+  int i = 0;
+  for (i = 0; i < 10; ++i) {
+    printf("iteration is %d\n", i);
+  }
+  return 0;
 }
